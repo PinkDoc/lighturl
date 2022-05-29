84a3d88399d62a58683f8118c0bc3a34beb0f84b
Pink <996234439@qq.com>
2022-05-29 11:14:13 +0800

    add example

---
+++ example.cc
@@ -0,0 +1,37 @@
+#include "url.hpp"
+#include <iostream>
+
+
+int main()
+{
+    using namespace lighturl;
+    url hu, fu;
+    std::string http = "http://localhost:8080/index.html#p3";
+    std::string ftp = "ftp://pink:1234567@localhost:2000/lover.mp3";
+    hu.parse(http);
+    fu.parse(ftp);
+
+    std::cout << fu.print() << std::endl;
+    std::cout << hu.print() << std::endl;
+    std::string s = hu.print();
+
+
+    // For networking
+    std::string buffer = "http:/";
+    url u;
+    auto len = buffer.size();
+    for (auto i = 0; i < buffer.size(); ++i)
+    {
+        u.parse(buffer[i]);
+    }
+
+    // buffer recv more data
+    buffer += "/192.168.1.1/index.htm";
+    for (auto i = len; i < buffer.size(); ++i)
+    {
+        u.parse(buffer[i]);
+    }
+
+    std::cout << u.print() << std::endl;
+}
+
